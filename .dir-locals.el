ca2de20caf70ca651cc27a7365721525b34a92ce
shynur <one.last.kiss@outlook.com>
2023-12-16 23:44:55 +0800

    ;

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((nil . ((eval . (when (buffer-file-name)
-                   (add-hook 'before-save-hook (lambda ()
-                                                 (save-excursion
-                                                   (add-file-local-variable 'coding 'utf-8-unix)))
-                             nil "buffer local"))))))
-
-;; Local Variables:
-;; coding: utf-8-unix
-;; End:
